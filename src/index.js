e32d6bc705a61dcec327baaf275276c9b346413f
Mpot7 <r-kisa@mail.ru>
2018-10-01 07:50:00 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function solveEquation(equation) {
+module.exports = function solveEquation(equation) 
+{
   // your implementation
+//var testeq="-464 * x^2 + 2224458688 * x - 998668448399424";
+var testeq=equation;
+testeq=testeq.replace(/\s+/g,"");
+testeq=testeq.replace(/\*x\^2/," ");
+testeq=testeq.replace(/\*x/," ");
+//console.log(testeq);
+var a=parseInt(testeq);
+//console.log(" ");
+
+var arr=testeq.split(" ");
+//console.log (arr);
+var a=parseInt(arr[0]);
+var b=parseInt(arr[1]);
+var c=parseInt(arr[2]);
+//console.log(" ");
+//console.log(a,b,c);
+var D=b*b-4*a*c;
+var x=[];
+x[0]=Math.round((-b+Math.sqrt(D))/(2*a));
+x[1]=Math.round((-b-Math.sqrt(D))/(2*a));
+if (x[0]>x[1])
+{
+m=x[0];
+x[0]=x[1];
+x[1]=m;
+}
+ 
+//console.log(x);
+return x;
+
 }
